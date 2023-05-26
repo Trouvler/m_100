1474553f6b116b9b6f569dc9cb2911c66d851bb6
Trouvler <60819676+Trouvler@users.noreply.github.com>
2023-05-26 11:19:07 +0900

    release

--- m100_user_app/android/app/src/main/kotlin/com/example/m_100/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.m_100
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ m100_user_app/android/app/src/main/kotlin/com/jnuMO/m_100/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jnuMO.m_100
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
